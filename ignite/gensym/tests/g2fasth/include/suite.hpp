aefa342ffbce4c5afb7e1731d68450f4ebee2f5d
Vladimir Ananiev <vladimir.ananiev@aurea.com>
2017-06-30 13:43:44 +0100

    update

--- ignite/gensym/tests/g2fasth/include/suite.hpp
+++ ignite/gensym/tests/g2fasth/include/suite.hpp
@@ -91,7 +91,10 @@ public:
             int elapsed = int(double(clock() - start) / CLOCKS_PER_SEC * 1000 + 0.5);
             int sleep = 10000 - elapsed;
             if (sleep > 0)
+            {
+                printf("Wait % ms...\n", sleep);
                 tthread::this_thread::sleep_for(tthread::chrono::milliseconds(sleep));
+            }
         }
 #endif
     }
