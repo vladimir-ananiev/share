a6cd57c02a82cc4d0194d1913b239a4fd63d83c5
Vladimir Ananiev <vladimir.ananiev@aurea.com>
2017-10-01 11:45:08 +0300

    update

--- lyris/tests/str_hash/str_hash.cpp
+++ lyris/tests/str_hash/str_hash.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string>
 
-class  os_string_hasher2 
+class  os_string_hasher2_my
 {
 public:
-	os_string_hasher2( long p = 1073741827 ) : prime_( p ) {}
+	os_string_hasher2_my( long p = 1073741827 ) : prime_( p ) {}
 	long operator()( const std::string& c ) const
 	{
 		int n = c.length();
@@ -21,6 +21,31 @@ protected:
 	int prime_;
 };
 
+typedef std::string os_string;
+
+class  os_string_hasher2 
+  {
+  public:
+    os_string_hasher2( long p = 1073741827 ) : prime_( p ) {}
+    long operator()( const os_string& c ) const;
+
+  protected:
+    long prime_;
+  };
+long os_string_hasher2::operator()( const os_string& c ) const
+  {
+  int n = c.length();
+  const char* d = c.data();
+  int h = n; 
+  int prime = (int)prime_;
+      
+  for ( int i = 0; i < n; ++i, ++d )
+    h = 613*h + *d;
+
+  return ((h >= 0) ? (h % prime) : (-h % prime)); 
+  }
+
+
 
 int main(int argc, char* argv[])
 {
@@ -28,13 +53,13 @@ int main(int argc, char* argv[])
 	int long_size = sizeof(long);
 	int long_int_size = sizeof(long int);
 	int long_long_size = sizeof(long long);
-	int void_ptr = sizeof(void*);
+	int void_ptr_size = sizeof(void*);
 
 	printf("int size = %d\n", int_size);
 	printf("long size = %d\n", long_size);
 	printf("long int size = %d\n", long_int_size);
 	printf("long long size = %d\n", long_long_size);
-	printf("void* size = %d\n", void_ptr);
+	printf("void* size = %d\n", void_ptr_size);
 
 	std::string in_str = "test strin jbdkjqdbwkjqwndkjqwnkdj nkjn kjqwdn kqnk nqkwdnkjn kjwqdn kjn knwd qkqjnwd kjqwndkjqwn kdg";
 
